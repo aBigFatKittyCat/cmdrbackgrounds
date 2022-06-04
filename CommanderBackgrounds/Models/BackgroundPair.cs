c560dae815dd96d18e35e1e3bc411ee44706ff15
Peter White <pwhite333@gmail.com>
2022-06-04 12:52:47 -0500

    it's legit working now

---
+++ CommanderBackgrounds/Models/BackgroundPair.cs
@@ -0,0 +1,11 @@
+namespace CommanderBackgrounds
+{
+    public class BackgroundPair
+    {
+        public string CommanderName { get; set; }
+        public string CommanderImageUri { get; set; }
+
+        public string BackgroundName { get; set; }
+        public string BackgroundImageUri { get; set; }
+    }
+}
--- CommanderBackgrounds/Program.cs
+++ CommanderBackgrounds/Program.cs
@@ -1,23 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CommanderBackgrounds;
+using CsvHelper;
+using System.Globalization;
 
 var client = new ScryFallClient();
 
 var commanders = await client.SearchCards("is%3Acommander+o%3A\"choose + a + background\"+-\"faceless + one\"");
 var backgrounds = await client.SearchCards("t%3Abackground+-\"faceless + one\"");
 
-var colors = new string[] { "B", "W", "U", "R", "G" };
+var colors = new Dictionary<string, Tuple<string, string>>();
+colors.Add("MONOBLACK", new("B", "B"));
 
-Console.WriteLine("Commanders:");
-Console.WriteLine("------------");
-foreach (var card in commanders.data)
+List<BackgroundPair> records;
+
+var configuration = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture);
+configuration.Delimiter = "\t";
+
+foreach (var color in colors.Keys)
 {
-    Console.WriteLine($"{card.name}");
+    records = new();
+    foreach (var commander in commanders.data.Where(c => c.color_identity.First() == colors[color].Item1))
+    {
+        foreach (var background in backgrounds.data.Where(c => c.color_identity.First() == colors[color].Item2))
+        {
+            records.Add(new()
+            {
+                CommanderName = commander.name,
+                CommanderImageUri = $"=IMAGE(\"{commander.image_uris["large"]}\", 3)",
+                BackgroundName = background.name,
+                BackgroundImageUri = $"=IMAGE(\"{background.image_uris["large"]}\", 3)"
+
+            });
+        }
+
+    }
+    using (var writer = new StreamWriter($"C:\\Users\\Peter\\Documents\\commanderbackgrounds\\{color}.csv"))
+    using (var csv = new CsvWriter(writer, configuration))
+    {
+        csv.WriteRecords(records);
+    }
 }
 
-Console.WriteLine("Backgrounds:");
-Console.WriteLine("------------");
-foreach (var card in backgrounds.data)
-{
-    Console.WriteLine($"{card.name}");
-}
